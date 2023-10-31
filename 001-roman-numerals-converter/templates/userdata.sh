f5373911021cc10d2581be368ba14eca81d9754d
Patrick <patrick.amagne@gmail.com>
2023-10-30 20:21:08 -0400

    fix flask run mode

--- 001-roman-numerals-converter/templates/userdata.sh
+++ 001-roman-numerals-converter/templates/userdata.sh
@@ -2,4 +2,17 @@
 
 yum update -y
 yum install python3 -y
+yum install pip -y
 pip3 install flask
+
+FOLDER="https://raw.githubusercontent.com/amagne/my-repository/main/001-roman-numerals-converter"
+
+cd /home/ec2-user
+wget ${FOLDER}/app.py
+mkdir templates
+cd templates
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html
+cd ..
+
+py
